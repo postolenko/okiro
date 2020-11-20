a1975ffa94d550ec0e9a63d5715eddbc767ac7e5
postolenko <denis.postolenko@gmail.com>
2020-11-20 15:52:08 +0200

    Okiro

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # http://postolenko.github.io/okiro/politic.html
 # http://postolenko.github.io/okiro/remont.html
+# http://postolenko.github.io/okiro/delivery.html
--- js/scripts.js
+++ js/scripts.js
@@ -62,4 +62,46 @@ $(document).ready(function() {
       }
     });
 
+    // ----------------------
+
+    ymaps.ready(function () {
+        if($("#map").length > 0) {
+            var myMap = new ymaps.Map('map', {
+                center: [55.755814, 37.617635],
+                zoom: 14
+            }, {
+                searchControlProvider: 'yandex#search'
+            });
+            myPlacemark1 = new ymaps.Placemark([55.755814, 37.617635], {
+                hintContent: ''
+            }, {
+                // iconLayout: 'default#imageWithContent',
+                // iconImageHref: 'img/yellow_marker.png',
+                // iconImageSize: [39, 35],
+                // iconImageOffset: [19, -17]
+            });
+            myMap.geoObjects.add(myPlacemark1);
+        }
+    });
+
+    ymaps.ready(function () {
+        if($("#map2").length > 0) {
+            var myMap2 = new ymaps.Map('map2', {
+                center: [55.755814, 37.617635],
+                zoom: 14
+            }, {
+                searchControlProvider: 'yandex#search'
+            });
+            myPlacemark2 = new ymaps.Placemark([55.755814, 37.617635], {
+                hintContent: ''
+            }, {
+                // iconLayout: 'default#imageWithContent',
+                // iconImageHref: 'img/yellow_marker.png',
+                // iconImageSize: [39, 35],
+                // iconImageOffset: [19, -17]
+            });
+            myMap2.geoObjects.add(myPlacemark2);
+        }
+    });
+
 });
